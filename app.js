e4fdb74756d93b621b1c9de9a837128657837818
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-11-11 13:49:59 +0000

    Port and IP setup

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function () {
     console.log("app running on port.", server.address().port);
 });
