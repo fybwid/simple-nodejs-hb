dd9d4d8e881ade89501d16361e19a9e826a58318
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-11-11 13:53:51 +0000

    Update text status

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 // Anytime there is GET request Server print "Welcome to fybwid restful API"
 var appRouter = function (app) {
   app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
+    res.status(200).send("Welcome to fybwid restful API");
   });
 }
 
